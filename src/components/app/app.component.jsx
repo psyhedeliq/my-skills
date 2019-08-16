24f105b265bb7a1c2701d8e44c3cdc2de1603f5b
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-16 14:43:59 +0100

    create skill component

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SkillSet from '../skill-set';
+import Skill from '../skill';
 
 const MySkills = () => {
   return (
     <div>
       <SkillSet />
+      <Skill />
     </div>
   );
 };
---
+++ src/components/skill/skill.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Skill = () => {
+  return (
+    <div>
+      <ul>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skill;
