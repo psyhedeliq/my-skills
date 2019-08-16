48078dd0e0761ce4b1127cf193f9267ae5b286ac
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-16 14:34:33 +0100

    cleanup on aisle public and src, add personal logo, description and tructure

---
+++ src/components/app/app.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MySkills = () => {
+  return <h1>MySkills.com</h1>;
+};
+
+export default MySkills;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MySkills from './components/app';
+
+const target = document.querySelector('#abracadabra');
+
+render(<MySkills />, target);
